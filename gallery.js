a42057989370cf78bcb248d766ff60c7db81345b
mrclputra <marcelino.ares.putra@gmail.com>
2026-06-10 11:40:19 +0800

    some more stuff

---
+++ gallery.js
@@ -0,0 +1,23 @@
+const pairs = new Map();
+
+document.querySelectorAll('[data-pair]').forEach(img => {
+    const key = img.dataset.pair;
+    if (!pairs.has(key)) pairs.set(key, []);
+    pairs.get(key).push(img);
+});
+
+[...pairs.values()].forEach((imgs, index) => {
+    let i = 0;
+    function next() {
+        // gifs should be longer
+        // const delay = 4000;
+        const delay = imgs[i].src.endsWith('.gif') ? 8000 : 3000;
+        setTimeout(() => {
+            imgs[i].style.opacity = '0';
+            i = (i + 1) % imgs.length;
+            imgs[i].style.opacity = '1';
+            next();
+        }, delay);
+    }
+    setTimeout(next, (index + 1) * 500);
+});
