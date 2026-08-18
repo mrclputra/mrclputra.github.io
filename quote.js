7c0623aab8fb16cc39d3d2f5897421c6f266a4ca
mrclputra <marcelino.ares.putra@gmail.com>
2026-08-18 14:57:42 +0800

    bow ashigaru

--- quote.js
+++ quote.js
@@ -3,10 +3,10 @@ const text = document.getElementById('quote-text');
 
 const versions = [
     'If you ever go out the back door, remember to water the flowers',
-    'If you ever go out the <b>front</b> door, remember to water the flowers',
+    'If you ever go out the back door, remember to water the <b>garden</b>',
     'If you ever go out the <b>front</b> door, remember to water the <b>garden</b>',
     'If you <b>never</b> go out the <b>front</b> door, remember to water the <b>garden</b>',
-    'If you are still inside, maybe you never left?'
+    'If you are still inside, did you ever leave?'
 ];
 
 let toggleCount = 0;
